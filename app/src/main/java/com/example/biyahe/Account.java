c6bbd43142c0072449db9fe66ed72f3c34aaa2db
khaira123 <tarawikhaira@gmail.com>
2022-12-06 12:02:02 +0800

    Updated Account GUI

--- app/src/main/java/com/example/biyahe/Account.java
+++ app/src/main/java/com/example/biyahe/Account.java
@@ -1,15 +1,31 @@
 package com.example.biyahe;
 
+import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.UserProfileChangeRequest;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class Account extends AppCompatActivity {
 
     BottomNavigationView navigationView;
+    @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -21,21 +37,21 @@ public class Account extends AppCompatActivity {
 
         navigationView.setOnItemSelectedListener(item -> {
 
-            switch (item.getItemId()){
+            switch (item.getItemId()) {
 
                 case R.id.home:
                     startActivity(new Intent(getApplicationContext()
-                    ,Home.class));
+                            , Home.class));
                     return true;
 
                 case R.id.sos:
                     startActivity(new Intent(getApplicationContext()
-                            ,SOS.class));
+                            , SOS.class));
                     return true;
 
                 case R.id.account:
                     startActivity(new Intent(getApplicationContext()
-                            ,Account.class));
+                            , Account.class));
                     return true;
             }
             return true;
